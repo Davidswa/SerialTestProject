4fc56c1050bf43cc6c655a06e0e7bc61ff7ed4c6
Davidswa <david.swartzen@gmail.com>
2017-03-20 22:13:30 -0700

    It works! for a second or two...

--- src/root/nested/SerialDisplay.py
+++ src/root/nested/SerialDisplay.py
@@ -5,15 +5,14 @@ Created on Mar 11, 2017
 '''
 import serial
 import matplotlib.pyplot as plt
-from collections import deque
+
 
 #Initialize variables
 xVal = []
 yVal = []
 zVal = []
-#t = []
-d = deque([],maxlen=5000)
-t = deque([],maxlen=5000)
+t = []
+
 
 #Setup Serial here
 ser = serial.Serial('COM8',38400)
@@ -38,13 +37,10 @@ def update(temp):
     xVal.append(int(temp[1]))
     yVal.append(int(temp[2]))
     zVal.append(int(temp[3]))
-    #t.append(len(zVal)) #Add to "time" or x-axis
-    
-    d.append(int(temp[3]))
-    t.append(range(0,len(zVal)))
+    t.append(len(zVal)) #Add to "time" or x-axis
     
     ln.set_data(t,zVal)
-    print ln.get_xdata()
+    
     return ln
 
 while True:
@@ -52,9 +48,10 @@ while True:
         pass
     datIn = update(ser.readline())
     
-    fig.canvas.draw()
-    ax.relim()
-    ax.autoscale_view()
-    #plt.show(block=False)
+    if len(t) % 1000:
+        fig.canvas.draw()
+        ax.relim()
+        ax.autoscale_view()
+        plt.show(block=False)
     
 ser.close()
