dd697d5de5fbb94ea76b0850509d343d67faa0d2
Davidswa <david.swartzen@gmail.com>
2017-04-14 22:01:31 -0700

    Close to getting this to work. I was able to get a graph to plot very slowly, but in order to try and make the graph truely real time, I am using the animation module. There is currently a TypeError: 'Line2D' is not an iterable

--- src/root/nested/SerialDisplay.py
+++ src/root/nested/SerialDisplay.py
@@ -4,7 +4,9 @@ Created on Mar 11, 2017
 @author: David
 '''
 import serial
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 
 #Initialize variables
@@ -12,7 +14,6 @@ xVal = []
 yVal = []
 zVal = []
 t = []
-
 
 #Setup Serial here
 ser = serial.Serial('COM8',38400)
@@ -23,35 +24,43 @@ for x in range(0,3):
     print(ser.readline())
 
 #Setup plot
+print("Starting figure setup\n")
 fig, ax = plt.subplots()
+line, = ax.plot(t, zVal)
+ax.set_ylim(-65500,65500)
+ax.set_xlim(0,1000)
 plt.title('Real-time Acceleration')
-plt.ion()
-ln, = ax.plot(t,zVal,'r-')
-plt.show()
 
 print("Entering drawing")
 
-def update(temp):
-   
+def update(i,temp):
+    print("Entered update\n")
     temp = temp.split("\t") #split up tabs
     xVal.append(int(temp[1]))
     yVal.append(int(temp[2]))
     zVal.append(int(temp[3]))
+
     t.append(len(zVal)) #Add to "time" or x-axis
     
-    ln.set_data(t,zVal)
+    line.set_data(t, zVal)
     
-    return ln
+    return line
 
-while True:
-    while ser.in_waiting == 0: #wait for incoming data
-        pass
-    datIn = update(ser.readline())
+def init():
+    line.set_data(t, zVal)
+    print("initialize complete\n")
+    return line
+
+def frameSource():
+    print("frameSource called\n")
+    return ser.readline()
     
-    if len(t) % 1000:
-        fig.canvas.draw()
-        ax.relim()
-        ax.autoscale_view()
-        plt.show(block=False)
-    
+ani = animation.FuncAnimation(fig, 
+                              update,
+                              init_func=init,
+                              fargs=(frameSource), 
+                              interval=0.01, 
+                              blit=True)
+plt.show()
+
 ser.close()
